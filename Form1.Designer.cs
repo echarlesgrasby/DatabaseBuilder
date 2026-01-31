d1fd4854faa263fc0d5af82566a18e94111732a2
Eric Charles Grasby, MSIQ <ericcgrasby@gmail.com>
2026-01-31 13:22:47 -0600

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace DatabaseBuilder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            infoToolStripMenuItem = new ToolStripMenuItem();
+            applicationInfoToolStripMenuItem = new ToolStripMenuItem();
+            citationToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, infoToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // infoToolStripMenuItem
+            // 
+            infoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { applicationInfoToolStripMenuItem, citationToolStripMenuItem });
+            infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            infoToolStripMenuItem.Size = new Size(40, 20);
+            infoToolStripMenuItem.Text = "Info";
+            infoToolStripMenuItem.Click += infoToolStripMenuItem_Click;
+            // 
+            // applicationInfoToolStripMenuItem
+            // 
+            applicationInfoToolStripMenuItem.Name = "applicationInfoToolStripMenuItem";
+            applicationInfoToolStripMenuItem.Size = new Size(180, 22);
+            applicationInfoToolStripMenuItem.Text = "Application Info";
+            applicationInfoToolStripMenuItem.Click += applicationInfoToolStripMenuItem_Click;
+            // 
+            // citationToolStripMenuItem
+            // 
+            citationToolStripMenuItem.Name = "citationToolStripMenuItem";
+            citationToolStripMenuItem.Size = new Size(180, 22);
+            citationToolStripMenuItem.Text = "Citation";
+            citationToolStripMenuItem.Click += citationToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "DatabaseBuilder - Eric C Grasby - Dissertation Support - Jan 2026";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem infoToolStripMenuItem;
+        private ToolStripMenuItem applicationInfoToolStripMenuItem;
+        private ToolStripMenuItem citationToolStripMenuItem;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+namespace DatabaseBuilder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void citationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void applicationInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("DatabaseBuilder Application\nVersion: v0.1\nAuthor: Eric C. Grasby, MSIQ\n\n" +
+                            "This application is built in support of a dissertation (A Domain-Specific Language" +
+                            "Approach to Monitoring and Surveillance Within Wholesale Electricity Markets)");
+        }
+    }
+}
